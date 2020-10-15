cd3358ca526c0b2c8dd4d71e46a46b8585590068
antonioo-c <72902999+antonioo-c@users.noreply.github.com>
2020-10-15 18:54:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ my_pass = 'fujkixpkjiyhcaji'  # 发件人邮箱密码
 my_user = 'anton1554970211@126.com'  # 收件人邮箱账号，我这边发送给自己
 
 
-def mail(img):
+def mail():
     ret = True
     try:
         msgRoot = MIMEMultipart('related')
@@ -38,19 +38,10 @@ def mail(img):
         mail_msg = """
         <p>Python 邮件发送测试...</p>
         <p>图片演示：</p>
-        <p><img src="cid:image1"></p>
+        
         """
         msgAlternative.attach(MIMEText(mail_msg, 'html', 'utf-8'))
-
-        # 指定图片为当前目录
-        fp = open(img, 'rb')
-        msgImage = MIMEImage(fp.read())
-        fp.close()
-
-        # 定义图片 ID，在 HTML 文本中引用
-        msgImage.add_header('Content-ID', '<image1>')
-        msgRoot.attach(msgImage)
-
+        
         server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
         server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
         server.sendmail(my_sender, [my_user, ], msgRoot.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
@@ -58,7 +49,6 @@ def mail(img):
     except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
         ret = False
     return ret
-
 
 TIMEOUT = 10
 TIMESLP = .5
@@ -286,7 +276,7 @@ def run(driver, username, password, campus, reason, destination, track,
     img = screen_capture(driver)
     print('=================================')
      
-    mail(img)
+    mail()
     print('可以愉快的玩耍啦！')
 
 
